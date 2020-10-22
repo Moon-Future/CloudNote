16bcde7e1a8cc84bc714703dee6bc88b438ab174
Leon <236338364@qq.com>
2020-10-22 19:00:35 +0800

    node

---
+++ 99.Random/index.js
@@ -0,0 +1,30 @@
+/**
+ * 随机提问
+ * ../3.面试题集锦/99.TODO.md
+ */
+
+const fs = require('fs')
+const path = require('path')
+const file = '../3.面试题集锦/99.TODO.md'
+const NUM = 10
+
+function getRandom(min, max) {
+  return Math.floor( Math.random() * (max - min + 1) + min)
+}
+
+let fileContent = fs.readFileSync(path.resolve(__dirname, file), 'utf-8')
+let fileArr = []
+fileContent.split('\n\r').forEach(ele => {
+  ele.trim()
+  ele !== '' ? fileArr.push(ele) : false
+})
+
+let result = []
+for (let i = 0; i < NUM; i++) {
+  let index = getRandom(0, fileArr.length - 1)
+  let item = fileArr.splice(index, 1)
+  console.log(item)
+  result.push(`${i + 1}. ${item.join('')}`)
+}
+
+console.log(result.join(''))
